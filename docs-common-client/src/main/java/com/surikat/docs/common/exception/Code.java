7469fbe7d8d49f80b198d6fa8cb3998421530ddb
Surikat12 <Surikat12@yandex.ru>
2024-06-08 13:14:25 +0300

    Added base exceptions Added DocsServiceException and DocsServiceRuntimeException

---
+++ docs-common-client/src/main/java/com/surikat/docs/common/exception/Code.java
@@ -0,0 +1,7 @@
+package com.surikat.docs.common.exception;
+
+public interface Code {
+
+    int getValue();
+    String getDescription();
+}
---
+++ docs-common-client/src/main/java/com/surikat/docs/common/exception/DocsServiceException.java
@@ -0,0 +1,76 @@
+package com.surikat.docs.common.exception;
+
+public class DocsServiceException extends Exception {
+
+    private final Code code;
+    private Integer httpStatus;
+
+    public DocsServiceException() {
+        this(ErrorCode.SERVER_ERROR);
+    }
+
+    public DocsServiceException(Code code) {
+        super();
+        this.code = code;
+    }
+
+    public DocsServiceException(Code code, String msg) {
+        super(msg);
+        this.code = code;
+    }
+
+    public DocsServiceException(Code code, String msg, Throwable e) {
+        super(msg, e);
+        this.code = code;
+    }
+
+    public DocsServiceException(String msg) {
+        this(ErrorCode.SERVER_ERROR, msg);
+    }
+
+    public DocsServiceException(String msg, Throwable e) {
+        this(ErrorCode.SERVER_ERROR, msg, e);
+    }
+
+    public Integer getHttpStatus() {
+        return httpStatus;
+    }
+
+    public void setHttpStatus(Integer httpStatus) {
+        this.httpStatus = httpStatus;
+    }
+
+    public Code getCode() {
+        return code;
+    }
+
+    public int getErrorCode() {
+        return code.getValue();
+    }
+
+    public String getDescription() {
+        return this.getMessage() != null ? this.getMessage() : code.getDescription();
+    }
+
+    public enum ErrorCode implements Code {
+        SERVER_ERROR(1, "На сервере произошла ошибка");
+
+        private final int value;
+        private final String description;
+
+        ErrorCode(int value, String description) {
+            this.value = value;
+            this.description = description;
+        }
+
+        @Override
+        public int getValue() {
+            return value;
+        }
+
+        @Override
+        public String getDescription() {
+            return description;
+        }
+    }
+}
