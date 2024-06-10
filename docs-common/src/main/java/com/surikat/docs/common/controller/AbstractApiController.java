948a5e11a3a0ac2438f573faca9b6799649581e1
Surikat12 <Surikat12@yandex.ru>
2024-06-10 09:03:30 +0300

    Added logging Added log4j logging

--- docs-common/src/main/java/com/surikat/docs/common/controller/AbstractApiController.java
+++ docs-common/src/main/java/com/surikat/docs/common/controller/AbstractApiController.java
@@ -3,6 +3,8 @@ package com.surikat.docs.common.controller;
 import com.surikat.docs.common.exception.DocsServiceException;
 import com.surikat.docs.common.exception.DocsServiceRuntimeException;
 import com.surikat.docs.common.model.rest.ApiError;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,10 +14,13 @@ import java.lang.reflect.UndeclaredThrowableException;
 
 public abstract class AbstractApiController {
 
+    private static final Logger LOGGER = LogManager.getLogger();
+
     @ExceptionHandler(DocsServiceException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ResponseBody
     public ApiError handleDocsServiceException(DocsServiceException e) {
+        LOGGER.error("handleDocsServiceException", e);
         return new ApiError(e.getErrorCode(), e.getDescription());
     }
 
@@ -23,6 +28,7 @@ public abstract class AbstractApiController {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ResponseBody
     public ApiError handleDocsServiceRuntimeException(DocsServiceRuntimeException e) {
+        LOGGER.error("handleDocsServiceRuntimeException", e);
         return new ApiError(e.getErrorCode(), e.getDescription());
     }
 
@@ -30,6 +36,7 @@ public abstract class AbstractApiController {
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ResponseBody
     public ApiError handleException(Exception e) {
+        LOGGER.error("handleException (ErrorCode.SERVER_ERROR)", e);
         return new ApiError(DocsServiceException.ErrorCode.SERVER_ERROR.getValue(), e.toString());
     }
 
@@ -37,6 +44,7 @@ public abstract class AbstractApiController {
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ResponseBody
     public ApiError handleException(UndeclaredThrowableException e) {
+        LOGGER.error("handleException (ErrorCode.SERVER_ERROR)", e);
         return new ApiError(DocsServiceException.ErrorCode.SERVER_ERROR.getValue(), e.toString());
     }
 }
